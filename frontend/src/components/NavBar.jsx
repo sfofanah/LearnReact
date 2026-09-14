b1c28b149ead370342c1f75d15dae98aa85f9e46
sfofanah <sfofanah4@gmail.com>
2026-09-14 11:55:52 -0400

    Created a navbar component and added it to App.jsx

---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom"
+
+function NavBar()
+{
+    return <nav className="navbar">
+        <div className="navbar-brand">
+            <Link to="/">Move App</Link>
+        </div>
+
+        <div className="navbar-link">
+            <Link to="/" className="nav-link">Home</Link>
+            <Link to="/Favorites" className="nav-link">Favorites</Link>
+        </div>
+    </nav>
+}
+
+export default NavBar
